577e5a758fc67cd816dc3f7c810ae3a91efe2ad2
unknown <sajedehrad83@gmail.com>
2024-01-26 13:43:13 -0800

    APP project

--- src/APP.java
+++ src/APP.java
@@ -1,9 +1,16 @@
+import javax.swing.SwingUtilities;
 
 public class APP {
 	
-public static void main(String args[]) {
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				new FootballLeague().setVisible(true);
+                              }
+		                                          });
+	                                       }
+                }
+
 		
-	FootballLeague a = new FootballLeague();
-	}
-	
-}
